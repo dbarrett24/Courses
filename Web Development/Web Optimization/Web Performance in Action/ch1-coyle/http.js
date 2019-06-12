9503355cff8466fcd71c0e71b52b7772e4cb2ec8
dbarrett24 <dbarrett24@gmail.com>
2019-06-12 16:20:04 -0600

    ch1 70% speed improvement of site

--- Web Development/Web Optimization/Web Performance in Action/ch1-coyle/http.js
+++ Web Development/Web Optimization/Web Performance in Action/ch1-coyle/http.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var compression = require("compression");
 var app = express();
 
 // Run static server
+app.use(compression());
 app.use(express.static(__dirname));
 app.listen(8080);
