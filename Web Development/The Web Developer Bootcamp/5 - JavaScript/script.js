e9c2bcaac489b2f645dd57f26c766b881b835558
Dave Barrett <dbarrett24@gmail.com>
2019-12-11 08:47:53 -0700

    javascript section

---
+++ Web Development/The Web Developer Bootcamp/5 - JavaScript/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("What is your age?");
+alert("Your full name is firstName + " " + lastName");
+
+
