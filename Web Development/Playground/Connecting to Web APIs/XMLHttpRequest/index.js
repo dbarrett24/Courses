7c84bf3612c19132b1f6ab6ad450649c9720437e
David Barrett <dev.davebarrett@gmail.com>
2021-02-15 16:14:28 -0700

    listing products from an api endpoint

--- Web Development/Playground/Connecting to Web APIs/XMLHttpRequest/index.js
+++ Web Development/Playground/Connecting to Web APIs/XMLHttpRequest/index.js
@@ -1,11 +1,25 @@
 let request = new XMLHttpRequest();
 
-request.open('GET', 'https://hplussport.com/api/products');
+request.open("GET", "https://hplussport.com/api/products?&order=name");
 
-request.onload = function(){
-    let response = request.response;
-    let parsedData = JSON.parse(response);
-    console.log(parsedData);
-}
+request.onload = function () {
+  let response = request.response;
+  let parsedData = JSON.parse(response);
+  console.log(parsedData);
+
+  for (item in parsedData) {
+    //List items in products
+    let name = parsedData[item].name;
+    let products = document.createElement("li");
+    products.innerHTML = name;
+    document.body.appendChild(products);
+
+    //set images from that image on the api
+    let imageUrl = parsedData[item].image;
+    let images = document.createElement("img");
+    images.setAttribute("src", imageUrl);
+    document.body.appendChild(images);
+  }
+};
 
 request.send();
