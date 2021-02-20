e5fda1a4aea4e7a8a2ba76447b37ab31a1dd240e
David Barrett <dbarrett24@gmail.com>
2021-02-20 10:38:33 -0700

    seasons update

--- Web Development/React/react_redux_stephengrider/seasons/src/index.js
+++ Web Development/React/react_redux_stephengrider/seasons/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-class App extends React.Component{
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  //The constructor function is the first function that is called when this class is created.
+  constructor(props) {
+    //super is a reference to the parent class' constructor function.
+    super(props);
 
-        return <div>Latitude: </div>;
-    }
+    //this is the ONLY TIME we do direct assignment
+    //to this.state! NEVER directly assign things to state.
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(this);
+        //you have to call setState to update state.
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+      if(this.state.errorMessage && !this.state.lat){
+          return <div>Error: {this.state.errorMessage}</div>
+      }
+      if(this.state.errorMessage && this.state.lat){
+          return <div>Latitude: {this.state.lat}</div>
+      }
+      return <div>Loading!</div>
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
