98edc497e7c808f03b407f0b8ce825c2d35fb3d2
David Barrett <david.barrett@liveauctioneers.com>
2021-05-06 18:03:14 -0600

    progress on course, life-cycle methods and passing props, using state

--- Web Development/React/react_redux_stephengrider/seasons/src/SeasonDisplay.js
+++ Web Development/React/react_redux_stephengrider/seasons/src/SeasonDisplay.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const SeasonDisplay = () => {
-    return <div>SeasonDisplay</div>
-}
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter"
+      ? "Burr, it is chilly"
+      : "Let's hit the beach basketball courts!";
+  return <h1>{text}</h1>;
+};
 
 export default SeasonDisplay;
--- Web Development/React/react_redux_stephengrider/seasons/src/index.js
+++ Web Development/React/react_redux_stephengrider/seasons/src/index.js
@@ -1,37 +1,42 @@
+/* eslint-disable no-useless-constructor */
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
   //The constructor function is the first function that is called when this class is created.
   constructor(props) {
     //super is a reference to the parent class' constructor function.
+    //(The React.Component class inside of react is the parent of THIS class as it is being extended.)
     super(props);
 
     //this is the ONLY TIME we do direct assignment
     //to this.state! NEVER directly assign things to state.
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
 
+    //you can define in constructor function
+    // this.state = {
+    //   lat: null,
+    //   errorMessage: "",
+    // };
+  }
+
+  //you can ALSO define in constructor function
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(this);
-        //you have to call setState to update state.
-        this.setState({ lat: position.coords.latitude });
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-
   //React says we have to define render!!
   render() {
-      if(this.state.errorMessage && !this.state.lat){
-          return <div>Error: {this.state.errorMessage}</div>
-      }
-      if(this.state.errorMessage && this.state.lat){
-          return <div>Latitude: {this.state.lat}</div>
-      }
-      return <div>Loading!</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading!</div>;
   }
 }
 
