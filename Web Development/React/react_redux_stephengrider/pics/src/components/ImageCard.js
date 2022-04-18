7e4679c2355872670fcacd7d4a2daa9d873217ea
David Barrett <dev.davebarrett@gmail.com>
2022-04-18 14:40:04 -0600

    ref practice

--- Web Development/React/react_redux_stephengrider/pics/src/components/ImageCard.js
+++ Web Development/React/react_redux_stephengrider/pics/src/components/ImageCard.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = height / Math.ceil(10);
+
+    this.setState({ spans: spans });
+  };
+
   render() {
     const { alt_description, urls } = this.props.image;
     return (
-      <div>
-        <img src={urls.regular} alt={alt_description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={alt_description} />
       </div>
     );
   }
