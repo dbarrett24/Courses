6c0b65da389426fea10c161367aefb4aa4553b4d
Dave Barrett <dev.davebarrett@gmail.com>
2023-03-29 10:40:43 -0600

    dumb animal project

--- Web Development/React/animals/src/App.js
+++ Web Development/React/animals/src/App.js
@@ -1,9 +1,30 @@
-const App = () => {
-    const handleClick = () => {
-        console.log('Button was clicked');
-    }
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import './App.css';
 
-    return <div><button onClick={handleClick}>Add Animal</button></div>
-}
+const getRandomAnimal = () => {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+};
+
+const App = () => {
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">{renderedAnimals}</div>
+        </div>
+    );
+};
 
 export default App;
