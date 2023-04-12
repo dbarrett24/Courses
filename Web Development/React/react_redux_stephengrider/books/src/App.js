83334a5f187852e70f060a1b628e9893ecce3e4b
Dave Barrett <dev.davebarrett@gmail.com>
2023-04-12 08:56:51 -0600

    styling and form

--- Web Development/React/react_redux_stephengrider/books/src/App.js
+++ Web Development/React/react_redux_stephengrider/books/src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-    return <div>App</div>;
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        setBooks();
+    };
+
+    const editBook = () => {
+
+    };
+
+    const deleteBook = () => {
+
+    };
+
+    return <div><BookCreate onCreate={createBook} /></div>;
 }
 
 export default App;
